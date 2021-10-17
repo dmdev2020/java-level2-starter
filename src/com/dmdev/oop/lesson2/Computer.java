34b514a85d6cfb9bd398b5fae1058869f87a6a2a
dmdev2020 <dmdev2020@gmail.com>
2021-10-17 22:26:47 +0200

    added files from lesson 3.1

--- src/com/dmdev/oop/lesson2/Computer.java
+++ src/com/dmdev/oop/lesson2/Computer.java
@@ -5,6 +5,20 @@ public class Computer {
     int ssd = 500;
     int ram;
 
+    Computer() {
+        System.out.println("Я был создан");
+    }
+
+    Computer(int newSsd) {
+        ssd = newSsd;
+        ram = 999;
+    }
+
+    Computer(int ssd, int newRam) {
+        this.ssd = ssd;
+        ram = newRam;
+    }
+
     void load() {
         System.out.println("Я загрузился");
     }
@@ -12,5 +26,6 @@ public class Computer {
     void printState() {
         System.out.println("Ssd: " + ssd);
         System.out.println("Ram: " + ram);
+        System.out.println();
     }
 }
--- src/com/dmdev/oop/lesson2/ComputerRunner.java
+++ src/com/dmdev/oop/lesson2/ComputerRunner.java
@@ -3,10 +3,13 @@ package com.dmdev.oop.lesson2;
 public class ComputerRunner {
 
     public static void main(String[] args) {
-        int value = 5;
         Computer computer = new Computer();
-        computer.load();
-        System.out.println("Ram: " + computer.ram);
-        System.out.println("Ssd: " + computer.ssd);
+        computer.printState();
+
+        Computer computer2 = new Computer(1050);
+        computer2.printState();
+
+        Computer computer3 = new Computer(10000, 10000);
+        computer3.printState();
     }
 }

93d12037e33ce40fdf44f3c0d08f323eb8cfbe32
dmdev2020 <dmdev2020@gmail.com>
2021-10-18 10:46:42 +0200

    added files from lesson 13.2

--- src/com/dmdev/oop/lesson11/OopLessonRunner.java
+++ src/com/dmdev/oop/lesson11/OopLessonRunner.java
@@ -3,16 +3,25 @@ package com.dmdev.oop.lesson11;
 public class OopLessonRunner {
 
     public static void main(String[] args) {
+        double value = 2.5;
+        int intValue = (int) value;
+
         Computer laptop = new Laptop(new Ssd(250), new Ram(1024), 2);
         Computer mobile = new Mobile(new Ssd(125), new Ram(512));
 
-        loadComputers(laptop, mobile);
-        printInformation(new Computer[]{laptop, mobile});
+        Laptop laptop1 = new Laptop(new Ssd(5000), new Ram(10000), 5);
+
+//        loadComputers(laptop, mobile);
+        printInformation(new Computer[]{laptop, mobile, laptop1});
     }
 
     public static void printInformation(Computer[] computers) {
         for (Computer computer : computers) {
             computer.print();
+            if (computer instanceof Laptop) {
+                Laptop laptop = (Laptop) computer;
+                laptop.open();
+            }
             System.out.println();
         }
     }

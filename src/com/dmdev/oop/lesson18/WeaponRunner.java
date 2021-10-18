65a82ee2179680dbc00352cd96e6e116751224b4
dmdev2020 <dmdev2020@gmail.com>
2021-10-18 11:07:40 +0200

    added files from lesson 18.3

--- src/com/dmdev/oop/lesson18/WeaponRunner.java
+++ src/com/dmdev/oop/lesson18/WeaponRunner.java
@@ -1,6 +1,7 @@
 package com.dmdev.oop.lesson18;
 
 import com.dmdev.oop.lesson15.Archer;
+import com.dmdev.oop.lesson15.Hero;
 import com.dmdev.oop.lesson15.Warrior;
 import com.dmdev.oop.lesson18.weapon.Bow;
 import com.dmdev.oop.lesson18.weapon.Sword;
@@ -13,5 +14,14 @@ public class WeaponRunner {
 
         Warrior<Sword> warrior = new Warrior<>("Боромир", 10);
         warrior.setWeapon(new Sword());
+
+        printWeaponDamage(warrior);
+    }
+
+//                                       Warrior<----->
+    public static void printWeaponDamage(Hero<Sword> hero) {
+        Sword weapon = hero.getWeapon();
+        hero.setWeapon(new Sword());
+        System.out.println(hero.getWeapon().getDamage());
     }
 }

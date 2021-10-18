c1a489a7d67c2901809ef5bc11261d5c9733a9a7
dmdev2020 <dmdev2020@gmail.com>
2021-10-18 10:49:51 +0200

    added files from lesson 14.2

--- src/com/dmdev/oop/lesson11/OopLessonRunner2.java
+++ src/com/dmdev/oop/lesson11/OopLessonRunner2.java
@@ -6,12 +6,18 @@ public class OopLessonRunner2 {
         Printable laptop = new Laptop(new Ssd(250), new Ram(1024), 2);
         Printable mobile = new Mobile(new Ssd(125), new Ram(512));
 
-        print(laptop, mobile);
+        printWithRandom(laptop, mobile);
     }
 
     public static void print(Printable... objects) {
         for (Printable object : objects) {
             object.print();
+            System.out.println();
+        }
+    }
+    public static void printWithRandom(Printable... objects) {
+        for (Printable object : objects) {
+            object.printWithRandom();
             System.out.println();
         }
     }
--- src/com/dmdev/oop/lesson11/Printable.java
+++ src/com/dmdev/oop/lesson11/Printable.java
@@ -1,8 +1,20 @@
 package com.dmdev.oop.lesson11;
 
+import java.util.Random;
+
 public interface Printable {
 
     String SOME_VALUE = "Example";
+    Random RANDOM = new Random();
+
+    default void printWithRandom() {
+        System.out.println(generateRandom());
+        print();
+    }
 
     void print();
+
+    private int generateRandom() {
+        return RANDOM.nextInt();
+    }
 }

c23e0f7a1a9abfae5f123ca7b837c5f00ee6cf11
dmdev2020 <dmdev2020@gmail.com>
2021-10-19 09:17:37 +0200

    added files from lesson 19.3

---
+++ src/com/dmdev/oop/lesson19/ArrayListRunner.java
@@ -0,0 +1,22 @@
+package com.dmdev.oop.lesson19;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListRunner {
+
+//    [1, 2, 3, 4, 5]
+//    [1, 2, 3, 4, 5, 6, null]
+    public static void main(String[] args) {
+        List<Integer> integers = new ArrayList<>(5);
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+        integers.add(5);
+        integers.add(6);
+
+        System.out.println(integers.get(5));
+        System.out.println(integers.contains(40));
+    }
+}

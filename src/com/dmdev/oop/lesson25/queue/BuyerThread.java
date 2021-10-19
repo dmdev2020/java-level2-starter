46021da7b5a456b7e56c6418ab42b73833e2409e
dmdev2020 <dmdev2020@gmail.com>
2021-10-19 19:50:00 +0200

    added files from lesson 26.6

--- src/com/dmdev/oop/lesson25/queue/BuyerThread.java
+++ src/com/dmdev/oop/lesson25/queue/BuyerThread.java
@@ -1,36 +1,23 @@
 package com.dmdev.oop.lesson25.queue;
 
-import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
 
 public class BuyerThread implements Runnable {
 
-    private final Queue<Cashbox> cashboxes;
+    private final BlockingQueue<Cashbox> cashboxes;
 
-    public BuyerThread(Queue<Cashbox> cashboxes) {
+    public BuyerThread(BlockingQueue<Cashbox> cashboxes) {
         this.cashboxes = cashboxes;
     }
 
     @Override
     public void run() {
         try {
-            synchronized (cashboxes) {
-                while (true) {
-                    if (!cashboxes.isEmpty()) {
-                        Cashbox cashbox = cashboxes.remove();
-                        System.out.println(Thread.currentThread().getName() + " обслуживается в кассе " + cashbox);
-
-                        cashboxes.wait(5L);
-
-                        System.out.println(Thread.currentThread().getName() + " освобождаю кассу " + cashbox);
-                        cashboxes.add(cashbox);
-                        cashboxes.notifyAll();
-                        break;
-                    } else {
-                        System.out.println(Thread.currentThread().getName() + " ожидает свободную кассу");
-                        cashboxes.wait();
-                    }
-                }
-            }
+            Cashbox cashbox = cashboxes.take();
+            System.out.println(Thread.currentThread().getName() + " обслуживается в кассе " + cashbox);
+            Thread.sleep(5L);
+            System.out.println(Thread.currentThread().getName() + " освобождаю кассу " + cashbox);
+            cashboxes.add(cashbox);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/com/dmdev/oop/lesson25/queue/CashboxRunner.java
+++ src/com/dmdev/oop/lesson25/queue/CashboxRunner.java
@@ -1,15 +1,15 @@
 package com.dmdev.oop.lesson25.queue;
 
-import java.util.ArrayDeque;
 import java.util.List;
-import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CashboxRunner {
 
     public static void main(String[] args) throws InterruptedException {
-        Queue<Cashbox> cashboxes = new ArrayDeque<>(List.of(new Cashbox(), new Cashbox()));
+        BlockingQueue<Cashbox> cashboxes = new ArrayBlockingQueue<>(2, true, List.of(new Cashbox(), new Cashbox()));
 
         List<Thread> threads = Stream.of(
                 new BuyerThread(cashboxes),

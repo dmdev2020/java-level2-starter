1b2de5e910d1181511c95b973f3dc147a904e39c
dmdev2020 <dmdev2020@gmail.com>
2021-10-19 19:47:45 +0200

    added files from lesson 26.5

--- src/com/dmdev/oop/lesson26/pool/ThreadPoolDemo.java
+++ src/com/dmdev/oop/lesson26/pool/ThreadPoolDemo.java
@@ -1,15 +1,33 @@
 package com.dmdev.oop.lesson26.pool;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolDemo {
 
-    public static void main(String[] args) {
-        Executors.newSingleThreadExecutor();
-        Executors.newFixedThreadPool(5);
-        Executors.newCachedThreadPool();
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ScheduledExecutorService threadPool = Executors.newScheduledThreadPool(4);
+        threadPool.scheduleAtFixedRate(() -> System.out.println("OK!"), 2L, 4L, TimeUnit.SECONDS);
 
-        Executors.newScheduledThreadPool(3);
-        Executors.newWorkStealingPool();
+//        threadPool.shutdown();
+//        threadPool.awaitTermination(1L, TimeUnit.HOURS);
+    }
+
+    private static void test() throws InterruptedException, ExecutionException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+        Future<Integer> future = threadPool.submit(() -> {
+            Thread.sleep(2000L);
+            System.out.println("It's callable");
+            return 1;
+        });
+
+        System.out.println("Result: " + future.get());
+        threadPool.shutdown();
+        threadPool.awaitTermination(1L, TimeUnit.HOURS);
+        System.out.println("main end");
     }
 }

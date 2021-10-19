08eff0c4e8732a58bf62c1675dfef176d8421a13
dmdev2020 <dmdev2020@gmail.com>
2021-10-19 18:06:31 +0200

    added files from lesson 25.3

--- src/com/dmdev/oop/lesson25/ThreadDemo.java
+++ src/com/dmdev/oop/lesson25/ThreadDemo.java
@@ -4,14 +4,20 @@ public class ThreadDemo {
 
     public static void main(String[] args) {
         var simpleThread = new SimpleThread();
+        System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
+
         var runnableThread = new Thread(new SimpleRunnable(), "My Name");
         var lambdaThread = new Thread(() -> System.out.println("Hello from lambda: " + Thread.currentThread().getName()));
 
         simpleThread.start();
+        System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
+
         runnableThread.start();
         lambdaThread.start();
         try {
-            simpleThread.join();
+            simpleThread.join(100L);
+            System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
+
             runnableThread.join();
             lambdaThread.join();
         } catch (InterruptedException e) {

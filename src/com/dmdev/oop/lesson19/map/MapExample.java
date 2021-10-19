9b06593b40185fdb516da66a97889cdcfcc8496c
dmdev2020 <dmdev2020@gmail.com>
2021-10-19 09:34:11 +0200

    added files from lesson 19.9

--- src/com/dmdev/oop/lesson19/map/MapExample.java
+++ src/com/dmdev/oop/lesson19/map/MapExample.java
@@ -1,6 +1,5 @@
 package com.dmdev.oop.lesson19.map;
 
-
 import com.dmdev.oop.lesson19.person.Person;
 
 import java.util.HashMap;
@@ -11,11 +10,26 @@ public class MapExample {
     public static void main(String[] args) {
         Person ivan = new Person(1, "Ivan", "Ivanov");
         Person petr = new Person(2, "Petr", "Petrov");
+        Person sveta = new Person(1, "Sveta", "Svetikova");
 
         Map<Integer, Person> map = new HashMap<>();
         map.put(ivan.getId(), ivan);
         map.put(petr.getId(), petr);
+        map.putIfAbsent(sveta.getId(), sveta);
 
-        System.out.println(map.get(2));
+//        System.out.println(map.keySet());
+//        System.out.println(map.values());
+//        System.out.println(map.entrySet());
+//
+//        for (Person person : map.values()) {
+//            System.out.println(person.getFirstName());
+//        }
+        for (Map.Entry<Integer, Person> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + "   " + entry.getValue());
+        }
+        System.out.println(map.containsKey(2));
+        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+        System.out.println(map.getOrDefault(2, sveta));
     }
 }

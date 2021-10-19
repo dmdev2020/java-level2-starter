e8aa47620d70888935d853114151687847e8bdb8
dmdev2020 <dmdev2020@gmail.com>
2021-10-19 19:57:45 +0200

    added files from lesson 26.9

--- src/com/dmdev/oop/lesson25/queue/BuyerThread.java
+++ src/com/dmdev/oop/lesson25/queue/BuyerThread.java
@@ -1,23 +1,25 @@
 package com.dmdev.oop.lesson25.queue;
 
-import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Semaphore;
 
 public class BuyerThread implements Runnable {
 
-    private final BlockingQueue<Cashbox> cashboxes;
+    private final Semaphore cashboxes;
 
-    public BuyerThread(BlockingQueue<Cashbox> cashboxes) {
+    public BuyerThread(Semaphore cashboxes) {
         this.cashboxes = cashboxes;
     }
 
     @Override
     public void run() {
         try {
-            Cashbox cashbox = cashboxes.take();
-            System.out.println(Thread.currentThread().getName() + " обслуживается в кассе " + cashbox);
+            cashboxes.acquire();
+
+            System.out.println(Thread.currentThread().getName() + " обслуживается в какой-то кассе");
             Thread.sleep(5L);
-            System.out.println(Thread.currentThread().getName() + " освобождаю кассу " + cashbox);
-            cashboxes.add(cashbox);
+            System.out.println(Thread.currentThread().getName() + " освобождаю какую-то кассу");
+
+            cashboxes.release();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/com/dmdev/oop/lesson25/queue/CashboxRunner.java
+++ src/com/dmdev/oop/lesson25/queue/CashboxRunner.java
@@ -1,16 +1,15 @@
 package com.dmdev.oop.lesson25.queue;
 
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Semaphore;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CashboxRunner {
 
     public static void main(String[] args) throws InterruptedException {
-        BlockingQueue<Cashbox> cashboxes = new ArrayBlockingQueue<>(2, true, List.of(new Cashbox(), new Cashbox()));
-
+//        BlockingQueue<Cashbox> cashboxes = new ArrayBlockingQueue<>(2, true, List.of(new Cashbox(), new Cashbox()));
+        Semaphore cashboxes = new Semaphore(2);
         List<Thread> threads = Stream.of(
                 new BuyerThread(cashboxes),
                 new BuyerThread(cashboxes),

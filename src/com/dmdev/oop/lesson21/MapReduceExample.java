b97df1c175314dea0d14fd075a27613af011e63e
dmdev2020 <dmdev2020@gmail.com>
2021-10-19 10:38:03 +0200

    added files from lesson 21.5

--- src/com/dmdev/oop/lesson21/MapReduceExample.java
+++ src/com/dmdev/oop/lesson21/MapReduceExample.java
@@ -1,5 +1,6 @@
 package com.dmdev.oop.lesson21;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class MapReduceExample {
@@ -12,18 +13,23 @@ public class MapReduceExample {
      * [1000_000] -> age -> max
      */
     public static void main(String[] args) {
-        Stream.of(
-                new Student(18, "Ivan"),
-                new Student(23, "Petr"),
-                new Student(34, "Vasya"),
-                new Student(45, "Sveta"),
-                new Student(20, "Katya"),
-                new Student(68, "Den"),
-                new Student(101, "Kira")
-        )
+        Optional<Student> maybeStudent = Stream.of(
+                        new Student(18, "Ivan"),
+                        new Student(23, "Petr"),
+                        new Student(34, "Vasya"),
+                        new Student(45, "Sveta"),
+                        new Student(20, "Katya"),
+                        new Student(68, "Den"),
+                        new Student(101, "Kira")
+                )
                 .sequential()
-                .map(Student::getAge)
-                .reduce(Integer::sum)
-                .ifPresent(System.out::println);
+//                .flatMap(student -> student.getMarks().stream())
+//                .map(mark -> )
+//                .map(Student::getAge)
+                .reduce((student1, student2) -> student1.getAge() > student2.getAge() ? student1 : student2);
+
+        maybeStudent.map(Student::getAge)
+                .map(age -> null)
+                .get();
     }
 }

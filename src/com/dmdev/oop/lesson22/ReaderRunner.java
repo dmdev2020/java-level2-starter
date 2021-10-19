426e16b319e02ac1e93a702b3751be6f0fc8f7db
dmdev2020 <dmdev2020@gmail.com>
2021-10-19 11:10:02 +0200

    added files from lesson 22.6

--- src/com/dmdev/oop/lesson22/ReaderRunner.java
+++ src/com/dmdev/oop/lesson22/ReaderRunner.java
@@ -1,20 +1,21 @@
 package com.dmdev.oop.lesson22;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ReaderRunner {
 
     public static void main(String[] args) throws IOException {
-        File file = Path.of("resources", "test.txt").toFile();
-        try (BufferedReader fileReader = new BufferedReader(new FileReader(file))) {
-            String collect = fileReader.lines()
-                    .collect(Collectors.joining("\n"));
-            System.out.println(collect);
+        Path path = Path.of("resources", "test.txt");
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.forEach(System.out::println);
         }
+//        try (BufferedReader fileReader = new BufferedReader(new FileReader(file))) {
+//            String collect = fileReader.lines()
+//                    .collect(Collectors.joining("\n"));
+//            System.out.println(collect);
+//        }
     }
 }
--- src/com/dmdev/oop/lesson22/WriterRunner.java
+++ src/com/dmdev/oop/lesson22/WriterRunner.java
@@ -1,19 +1,21 @@
 package com.dmdev.oop.lesson22;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class WriterRunner {
 
     public static void main(String[] args) throws IOException {
-        File file = Path.of("resources", "writer.poem").toFile();
-        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(file, true))) {
-            fileWriter.append("Hello World!");
-            fileWriter.newLine();
-            fileWriter.append("Java");
-        }
+        Path path = Path.of("resources", "writer2.poem");
+//        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(path, true))) {
+        Files.write(path, List.of("Hello World!", "Java"));
+
+//        try (BufferedWriter fileWriter = Files.newBufferedWriter(path)) {
+//            fileWriter.append("Hello World!");
+//            fileWriter.newLine();
+//            fileWriter.append("Java");
+//        }
     }
 }
